f66b460a957b5edb2b2f43fee22c0d3da0a63d31
haroldZS <haroldzs1024@gmail.com>
2023-11-28 15:34:02 -0400

    Factory creational pattern

---
+++ creational/factory/factory.js
@@ -0,0 +1,56 @@
+class BaseCar {
+  showCost() {
+    throw new Error("Method not implemented!");
+  }
+}
+
+class MastodonCar extends BaseCar {
+  showCost() {
+    console.log("Mastodon Car Cost: 300.000$");
+  }
+}
+
+class RhinoCar extends BaseCar {
+  showCost() {
+    console.log("Rhino Car Cost: 500.000$");
+  }
+}
+
+class CarFactory {
+  makeCar() {
+    console.log("Method not implemented!");
+  }
+}
+
+class MastodonCarFactory extends CarFactory {
+  makeCar() {
+    return new MastodonCar();
+  }
+}
+
+class RhinoCarFactory extends CarFactory {
+  makeCar() {
+    return new RhinoCar();
+  }
+}
+
+function appFactory(factory) {
+  const car = factory.makeCar();
+  car.showCost();
+}
+
+// appFactory(new MastodonCarFactory());
+// appFactory(new RhinoCarFactory());
+
+function createFactory(type) {
+  const factories = {
+    mastodon: MastodonCarFactory,
+    rhino: RhinoCarFactory,
+  };
+
+  const Factory = factories[type];
+  return new Factory();
+}
+
+appFactory(createFactory("mastodon"));
+appFactory(createFactory("rhino"));
